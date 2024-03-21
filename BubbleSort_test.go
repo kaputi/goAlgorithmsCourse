236dcac61a49b26ac441f8413c800cbbb5afd1a1
Eduardo Lonighi <edu_loni3@hotmail.com>
2024-03-21 21:35:56 +0000

    pointer in BubbleSort

--- BubbleSort_test.go
+++ BubbleSort_test.go
@@ -7,10 +7,10 @@ import (
 func TestBubbleSort(t *testing.T) {
 	arr := []int{9, 3, 7, 4, 69, 420, 42}
 
-	sorted := BubbleSort(arr)
+	BubbleSort(&arr)
 
 	for i, v := range arr {
-		if v != sorted[i] {
+		if v != arr[i] {
 			t.Error("not sorted")
 		}
 	}
